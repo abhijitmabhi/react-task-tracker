508246e9ec1d2990b75784353e23bb28f88e8238
Abhijit Mondal Abhi <abhi@localhero.de>
2021-04-18 23:09:37 +0200

    Ready for Production

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,6 +26,12 @@ function App() {
     }
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask]);
+  }
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -32,17 +40,20 @@ function App() {
   //Toggle Reminder
 
   const togglReminder = (id) => {
-    setTasks(tasks.map((task) => 
-      task.id === id ? {...task, reminder: !task.reminder} : task
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, reminder: !task.reminder } : task
     ))
   }
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
       {
-        tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={togglReminder}/>) : ('No Tasks To Show')
+        showAddTask && <AddTask onAdd={addTask} />
       }
-      
+      {
+        tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={togglReminder} />) : ('No Tasks To Show')
+      }
+
     </div>
   );
 }
